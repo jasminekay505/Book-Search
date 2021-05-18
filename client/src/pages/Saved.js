a4486ca305554241df667c0cfdce050628013e6e
Jasmine Kay <jasminekay505@gmail.com>
2021-05-18 16:42:04 +0800

    fixing no results div

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -6,27 +6,60 @@ import SavedList from "../components/SavedList/SavedList";
 
 import API from "../utils/API";
 
-class Saved extends Component { 
-    state = { 
+class Saved extends Component {
+    state = {
         savedBooks: []
     };
 
-    componentDidMount = () => { 
+    componentDidMount = () => {
         this.getBooks();
     };
 
-    deleteBook = selectedBook => { 
+    deleteBook = selectedBook => {
         API.deleteBook(selectedBook.id)
-        .then(res =>  { 
-            console.log("Book deleted from DB!", res)
-            this.getBooks();
-        })
-        .catch(err => { 
-            console.log(err);
-        })
+            .then(res => {
+                console.log("Book deleted from DB!", res)
+                this.getBooks();
+            })
+            .catch(err => {
+                console.log(err);
+            })
     };
 
-    getBooks = () => { 
+    getBooks = () => {
         API.getBooks()
+            .then(res => {
+                this.setState({
+                    savedBooks: res.data
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Container fluid>
+                    <Jumbotron />
+                    {this.state.savedBooks.length ? (
+                        <SavedList
+                            bookState={this.state.savedBooks}
+                            deleteBook={this.deleteBook}
+                        >
+                        </SavedList>
+                    ) : (
+                        <div>
+                            <hr />
+                            <p>No results to display</p>
+                        </div>
+                    )}
+                </Container>
+            </div>
+        )
     }
 }
+
+export default Saved
