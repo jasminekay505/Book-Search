0bbcfa917d1317e8341df25c61b60724714a1904
Jasmine Kay <jasminekay505@gmail.com>
2021-05-18 14:33:30 +0800

    Defining methods for bookController

---
+++ controllers/bookController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+//Define methods for bookController
+module.exports = {
+    findAll: function(req, res) {
+      db.Book
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    findOne: function(req, res) {
+      db.Book
+        .findOne(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    save: function(req, res) {
+      db.Book
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+      db.Book
+        .findOneAndUpdate({ _id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+      db.Book
+        .findOne({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+  };
